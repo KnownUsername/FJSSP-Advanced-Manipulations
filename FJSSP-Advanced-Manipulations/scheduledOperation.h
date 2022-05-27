1622df1474b3cac44377b4fe10f1696a2a62fcdd
KnownUsername <joaoftrodrigues@gmail.com>
2022-05-27 02:09:45 +0100

    Removed struct that was meant for list, to use generic lists #2 #4

--- FJSSP-Advanced-Manipulations/scheduledOperation.h
+++ FJSSP-Advanced-Manipulations/scheduledOperation.h
@@ -16,11 +16,21 @@ typedef struct ScheduledOperation {
 
 /// <summary>
 /// Defines a list of scheduled Operations
-/// </summary>
+/// </summary>	
 typedef struct ScheduledOperationList {
 	ScheduledOperation operation;
-	ScheduledOperationList* nextOperation;
+	struct ScheduledOperationList* nextOperation;
 }ScheduledOperationList;
 
+// Creates a ScheduledOperation with given arguments
+ScheduledOperation CreateScheduledOperation(int operationID, Process* process, int startTime, int endTime);
+
+
+// Compares a ScheduledOperation's id with a given id, 
+// provided from a generic list
+int CompareScheduledOperationId(void* operationData, void* id);
+
+// Verify if a ScheduledOperation exists on a list
+int ScheduledOperationExists(List* list, void* data);
 
 #endif
