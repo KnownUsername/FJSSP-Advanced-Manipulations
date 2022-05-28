87d43a4c532a1900e9de5c2f049b6dc100097f81
KnownUsername <joaoftrodrigues@gmail.com>
2022-05-28 19:25:08 +0100

    Script to generate Gantt with output csv files #3

---
+++ gantt.py
@@ -0,0 +1,28 @@
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+# Load data
+df = pd.read_csv('output_example.csv')
+
+# Add a column for the delta (difference between times)
+df['delta'] = df['end_time'] - df['start_time']
+
+# Assure string type
+df['job'] = df['job'].astype(str)
+
+# Setup Gantt
+fig = px.timeline(df, x_start="start_time", x_end="end_time", y="machine", range_x = (0,50), color='job')
+
+# To show machine's id ascendingly
+fig.update_yaxes(autorange="reversed")
+
+# Update the layout -> Allow int values for time, instead of timestamps
+fig.layout.xaxis.type = 'linear'
+
+# Fill data to each job, using delta values 
+for i in np.arange(len(df['job'].unique())):
+    fig.data[i].x = df.delta.tolist()
+
+# Save output as html file and open to view
+fig.write_html('first_figure.html', auto_open=True)
