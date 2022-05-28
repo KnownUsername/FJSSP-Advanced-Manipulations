6317a7edded1205de82005c5f8a118b90f93e4bd
KnownUsername <joaoftrodrigues@gmail.com>
2022-05-28 19:42:42 +0100

    Removed time restriction on x axes #3

--- gantt.py
+++ gantt.py
@@ -11,8 +11,9 @@ df['delta'] = df['end_time'] - df['start_time']
 # Assure string type
 df['job'] = df['job'].astype(str)
 
+
 # Setup Gantt
-fig = px.timeline(df, x_start="start_time", x_end="end_time", y="machine", range_x = (0,50), color='job', hover_data=['operation'])
+fig = px.timeline(df, x_start="start_time", x_end="end_time", y="machine", color='job', hover_data=['operation'])
 
 # To show machine's id ascendingly
 fig.update_yaxes(autorange="reversed")
