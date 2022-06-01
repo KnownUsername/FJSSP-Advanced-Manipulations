8f5336281bf606d8ea929bf93ba8dfcff5e5c165
KnownUsername <joaoftrodrigues@gmail.com>
2022-06-01 11:02:12 +0100

    Added methods to Create() and Compare() WeightedJobs

--- FJSSP-Advanced-Manipulations/weightedJob.c
+++ FJSSP-Advanced-Manipulations/weightedJob.c
@@ -1,3 +1,65 @@
 #include "weightedJob.h"
+#include "job.h"
+#include "weightedOperation.h"
+#include "weightedProcess.h"
 
+/// <summary>
+/// Creates a WeightedJob -> A Job containing Operations constituted by Processes with weights for measures
+/// </summary>
+/// <param name="job"></param>
+/// <param name="processPercent"></param>
+/// <returns></returns>
+WeightedJob CreateWeightedJob(Job job, float processPercent) {
 
+	WeightedJob weightedJob;
+	float averageTime, probHTAverage, difWorstTimeAvg, jobWeight, taxedJobWeight;
+	int maxDuration;
+
+	weightedJob.jobId = job.jobIdentifier;
+	
+	/*				Intermediate calculus		*/
+	averageTime = CalculateAverageOperationProcessTime(job.operations);
+	probHTAverage = ProbWorstThanAverage(job.operations, averageTime);
+	maxDuration = GetMaximumDurationOnOperationList(job.operations);
+	difWorstTimeAvg = maxDuration - averageTime;
+
+	jobWeight = averageTime + (difWorstTimeAvg * probHTAverage);
+
+	// Convert % value into a number from 0 to 1
+	processPercent = processPercent / 100;
+
+	taxedJobWeight = jobWeight * ((1-processPercent) / 100);
+
+	// Assignment to a variable with shorter name
+	OperationList* operations = job.operations;
+	WeightedOperation currentWeightedOperation;
+
+	while (operations) {
+		
+		// Build a WeightOperation from an Operation from the list
+		currentWeightedOperation = CreateWeightedOperation(operations->operation, job.jobIdentifier, processPercent, taxedJobWeight);
+
+		// Insert new WeightOperation on the list related to the current WeightedJob
+		weightedJob.weightedOperations = InsertListItem(weightedJob.weightedOperations, &currentWeightedOperation, CompareWeightedProcesses);
+	}
+
+	return weightedJob;
+}
+
+/// <summary>
+/// Compares a WeightedJob's id with a given id,
+/// provided from a generic list
+/// </summary>
+/// <param name="jobData"></param>
+/// <param name="ptrId"></param>
+/// <returns></returns>
+int CompareWeightedJobs(void* jobData, void* ptrId) {
+
+	// Data casts to desired type
+	WeightedJob* job = (WeightedJob*)jobData;
+	int id = (int)ptrId;
+
+	// Data comparisons
+	if (job->jobId == id) return 1;
+	else return 0;
+}
--- FJSSP-Advanced-Manipulations/weightedJob.h
+++ FJSSP-Advanced-Manipulations/weightedJob.h
@@ -2,10 +2,20 @@
 #define WEIGHT_JOB
 
 #include "list.h"
+#include "job.h"
 
+/// <summary>
+/// Defins a Job consituted by Processes with weights
+/// </summary>
 typedef struct WeightedJob {
 	char* jobId;
 	List* weightedOperations;
-};
+}WeightedJob;
 
+// Creates a WeightedJob -> A Job containing Operations constituted by Processes with weights for measures
+WeightedJob CreateWeightedJob(Job job, float processPercent);
+
+// Compares a WeightedJob's id with a given id,
+// provided from a generic list
+int CompareWeightedJobs(void* jobData, void* ptrId);
 #endif 
