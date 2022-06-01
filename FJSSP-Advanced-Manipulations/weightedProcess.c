245e6c977a2e6f1e6cb3a5c4cde2a607ed47613b
KnownUsername <joaoftrodrigues@gmail.com>
2022-06-01 11:10:51 +0100

    Added methods to Compare() and Show() WeightedProcesses; Disponibilized Create() method

--- FJSSP-Advanced-Manipulations/weightedProcess.c
+++ FJSSP-Advanced-Manipulations/weightedProcess.c
@@ -22,3 +22,44 @@ WeightedProcess CreateWeightedProcess(char* jobIdentifier, int operationId, Proc
 
 	return newWeightedProcess;
 }
+
+/// <summary>
+/// Compares a WeightedProcess's id with a given id,
+/// provided from a generic list
+/// </summary>
+/// <param name="data"></param>
+/// <param name="ptrId"></param>
+/// <returns></returns>
+int CompareWeightedProcesses(void* data, void* ptrId) {
+
+	// Data casts to desired type
+	WeightedProcess* process = (WeightedProcess*)data;
+	int id = (int)ptrId;
+
+	// Data comparisons
+	if (process->jobIdentifier == id) return 1;
+	else return 0;
+}
+
+/// <summary>
+/// Print values of a WeightedProcess from a generic list
+/// </summary>
+/// <param name="data"></param>
+void ShowWeightedProcess(void* data) {
+	
+	// Data type cast
+	WeightedProcess* weightedProcess = (WeightedProcess*)data;
+
+	// Empty data is discarded
+	if (!weightedProcess) return;
+
+	printf("Job: %s\n", weightedProcess->jobIdentifier);
+	printf("Operation: %d\n", weightedProcess->operationId);
+
+	// Print Process if exists
+	if(weightedProcess->process) ShowProcess(*weightedProcess->process);
+
+	printf("Reference Time: %d\n", weightedProcess->referenceTime);
+	printf("Cedency: %f\n", weightedProcess->cedency);
+
+}
--- FJSSP-Advanced-Manipulations/weightedProcess.h
+++ FJSSP-Advanced-Manipulations/weightedProcess.h
@@ -3,6 +3,9 @@
 
 #include "process.h"
 
+/// <summary>
+/// Defines a Process with weights
+/// </summary>
 typedef struct WeightedProcess
 {
 	char* jobIdentifier; // To keep id when using only structures' address
@@ -13,5 +16,10 @@ typedef struct WeightedProcess
 }WeightedProcess;
 
 
+// Creates a WeightedProcess -> A Process with weights for measures
+WeightedProcess CreateWeightedProcess(char* jobIdentifier, int operationId, Process* process, float processPercent, float jobFactor);
 
+// Compares a WeightedOperation's id with a given id,
+// provided from a generic list
+int CompareWeightedProcesses(void* data, void* ptrId);
 #endif 
