28a85e1d0b47bed872ee5213dc65e1d7fc1fe793
KnownUsername <joaoftrodrigues@gmail.com>
2022-06-06 16:59:01 +0100

    Updated with functions' call for tests (Same code as delivered)

--- FJSSP-Advanced-Manipulations/tests.c
+++ FJSSP-Advanced-Manipulations/tests.c
@@ -23,14 +23,39 @@ int main() {
 	int status;
 
 	// Load of a csv with multiple Jobs
-	List* jobs = LoadJobs("../template_data.csv", &status);
+	//List* jobs = LoadJobs("../template_data.csv", &status);
+
+	//ShowElements(jobs, ShowJob);
+
+	List* jobs = NULL;
+	Job job1 = CreateJob("first", NULL);
+
+	job1.operations = InsertOperation(job1.operations, CreateOperation(1, NULL));
+	job1.operations->operation.alternProcesses = InsertProcess(job1.operations->operation.alternProcesses, CreateProcess(1, 1));
+	job1.operations->operation.alternProcesses = InsertProcess(job1.operations->operation.alternProcesses, CreateProcess(3, 4));
+
+	Job job2 = CreateJob("second", NULL);
+
+	job2.operations = InsertOperation(job2.operations, CreateOperation(1, NULL));
+	job2.operations->operation.alternProcesses = InsertProcess(job2.operations->operation.alternProcesses, CreateProcess(1, 7));
+	job2.operations->operation.alternProcesses = InsertProcess(job2.operations->operation.alternProcesses, CreateProcess(2, 9));
+
+	jobs = InsertListItem(jobs, &job1, CompareJobs);
+	jobs = InsertListItem(jobs, &job2, CompareJobs);
 
 	ShowElements(jobs, ShowJob);
 
-	printf("%d", status);
-	List* weightedJobs = GenerateWeightedJobs(jobs, 60);
+	jobs = RemoveElement(jobs, job2.jobIdentifier, CompareJobs, DeleteJobDataFromList);
 
-	ShowElements(weightedJobs, ShowWeightedJob);
+	ShowElements(jobs, ShowJob);
+
+	job1.operations = RemoveOperation(job1.operations, 1);
+
+	ShowJob(job1);
+	//printf("%d", status);
+	//List* weightedJobs = GenerateWeightedJobs(jobs, 60);
+
+	//ShowElements(weightedJobs, ShowWeightedJob);
 
 	return 0;
 }
